8be98a398b2b72cd87a205aa32adf71dabe6edd2
nqthai <nathanqthai@users.noreply.github.com>
2019-05-22 15:00:32 -0400

    updated install

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# update submodules
 git submodule update --init --recursive
 
+# install YCM
 pushd vim/.vim/bundle/YouCompleteMe
 python3 install.py --clang-completer
 popd
+
+# install dotfiles
 for d in $(ls); do
     stow $d;
 done
