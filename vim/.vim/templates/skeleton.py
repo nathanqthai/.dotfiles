dc2af01b0e76ab82fb43c7c40fbcaeaa2952815b
nate <nathan.thai@idefense.com>
2019-11-10 15:19:46 -0500

    updated python skeleton

--- vim/.vim/templates/skeleton.py
+++ vim/.vim/templates/skeleton.py
@@ -2,15 +2,66 @@
 # vim: set ts=4 sw=4 tw=4 sts=4 et :
 
 import argparse
+import logging
+import logging.config
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+
+def init_logger(log_path):
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+        },
+        'handlers': {
+            'default_handler': {
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',
+                'formatter': 'standard',
+                'stream': sys.stdout
+            },
+            'file_handler': {
+                'class': 'logging.FileHandler',
+                'level': 'DEBUG',
+                'formatter': 'standard',
+                'filename': os.path.join('logs', 'application.log'),
+                'encoding': 'utf8'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default_handler', 'file_handler'],
+                'level': 'INFO',
+                'propagate': False
+            }
+        }
+    }
+    logging.config.dictConfig(logging_config)
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Template program.')
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Default')
+    parser.add_argument('--debug', help='debug', action='store_true')
+    parser.add_argument('-l', '--log_path', help='logs path', default='logs')
+    parser.add_argument('-i', '--input', help='input', default='input')
+    return parser.parse_args()
 
 def main():
-    parse_args()
+    args = parse_args()
 
-    print("Hello World");
+    # initialize logging
+    init_logger(args.log_path)
+    logging.info("Running {}".format(__file__))
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug("Debug mode enabled")
 
 if __name__ == "__main__":
     main()
